3372b57dce93f60c92821ca187a27b77d717269b
RJ <randalljo@gmail.com>
2016-11-30 23:05:26 -0700

    clean up

--- app/src/main/java/com/arejaysmith/tappydefender/TDView.java
+++ app/src/main/java/com/arejaysmith/tappydefender/TDView.java
@@ -12,10 +12,42 @@ public class TDView extends SurfaceView implements Runnable {
         super(context);
     }
 
+    volatile boolean playing;
+    Thread gameThread = null;
+
     @Override
     public void run() {
         while (playing) {
-            
+            update();
+            draw();
+            control();
         }
+
+    }
+
+    private void update() {
+
+    }
+
+    private void draw() {
+
+    }
+
+    private void control() {
+
+    }
+
+    public void pause() {
+        playing = false;
+        try {
+            gameThread.join();
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public void resume() {
+        playing = true;
+        gameThread = new Thread(this);
+        gameThread.start();
     }
 }
